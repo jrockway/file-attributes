912fdc688857b53319dc92049d8dddb7c7aa9f11
jon <jon@01d9642f-1d18-0410-bea3-844bd6f27a17>
2007-02-04 03:21:38 +0000

    r18072@foo:  jon | 2007-02-03 18:25:10 -0600  add a File::Attributes::Test module (and tests) to test dispatch/fallback/etc.
    git-svn-id: svn://svn.jrock.us/cpan_modules/File-Attributes@246 01d9642f-1d18-0410-bea3-844bd6f27a17

---
+++ t/lib/File/Attributes/Test.pm
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+# Test.pm 
+# Copyright (c) 2007 Jonathan Rockway <jrockway@cpan.org>
+# File::Attributes module for testing
+
+package File::Attributes::Test;
+use base 'File::Attributes::Base';
+our $VERSION = 
+  'foo-o-matic';
+
+my %attributes;
+
+# applicible unless filename contains "NONONO"
+sub applicable {
+    my $self = shift;
+    my $file = shift;
+    return 0 if $file =~ /NONONO/;
+    return 1;
+}
+
+# message from general pepper -- priority 1_000_000!
+sub priority { 1_000_000 }
+
+sub get {
+    return if $_[2] =~ /NONONO/; # ignore attributes that contain NONONO
+    return $attributes{$_[1]}->{$_[2]};
+}
+
+sub set {    
+    return if $_[2] =~ /NONONO/; # ignore
+    $attributes{$_[1]}->{$_[2]} = $_[3];
+}
+
+sub unset {
+    delete $attributes{$_[1]}->{$_[2]};
+}
+
+sub list {
+    return keys %{$attributes{$_[1]}};
+}
+
+1;
